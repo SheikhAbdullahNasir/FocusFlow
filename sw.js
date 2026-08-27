8892b8d4e9834b475ce789889e297f69a17f26e8
Abdullah Bin Nasir <abdullah.nasir7312@gmail.com>
2026-08-27 11:28:55 +0500

    feat: implement PWA support with service worker and offline assets

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'focusflow-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/timer.js',
+    './js/tasks.js',
+    './js/settings.js',
+    './js/theme.js',
+    './js/sounds.js',
+    './js/storage.js',
+    './icons/icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((cachedResponse) => {
+            return cachedResponse || fetch(e.request).catch(() => {
+                return caches.match('./index.html');
+            });
+        })
+    );
+});
